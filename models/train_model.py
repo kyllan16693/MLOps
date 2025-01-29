409be7045e6b6998934a7001e2b9c2d140c1af89
Kyllan Wunder <fencer4933@gmail.com>
2025-01-29 11:53:58 -0500

    New version of model with 2 features

--- models/train_model.py
+++ models/train_model.py
@@ -7,13 +7,13 @@ regdata = pd.read_csv('../data/sampregdata.csv')
 # Remove the first column, since it's just an index
 regdata = regdata.drop(columns=['Unnamed: 0'])
 
-# Only use x4
-best_features = ['x4']
+# Only use x2 and x4
+best_features = ['x2', 'x4']
 
 # Train the model using the best feature
 model = LinearRegression()
 model.fit(regdata[best_features], regdata['y'])
 
 
-print(f'Model trained using feature: {best_features}')
+print(f'Model trained using features: {best_features}')
 print(f'Model coefficients: {model.coef_}, Intercept: {model.intercept_}')
